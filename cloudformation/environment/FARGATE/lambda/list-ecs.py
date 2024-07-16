3f05857503491453e67c3baf1a2df5334f119708
Celso <celso@celsonunes.com.br>
2024-07-16 17:49:35 -0300

    update

--- cloudformation/environment/FARGATE/lambda/list-ecs.py
+++ cloudformation/environment/FARGATE/lambda/list-ecs.py
@@ -6,9 +6,9 @@ def lambda_handler(event, context):
     try:
         # Configurações do ECS
         client = boto3.client('ecs')
-        cluster_name = 'estudo-dev-cluster1'  # Nome do seu cluster
-        service_name = 'estudo-dev-app-game10'  # Nome do seu serviço
-        family = 'estudo-dev-app-game10'  # Nome da família da definição de tarefa
+        cluster_name = 'estudo-dev-cluster1'
+        service_name = 'estudo-dev-app-game10'
+        family = 'estudo-dev-app-game10'
         slack_webhook_url = 'https://hooks.slack.com/services/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'
 
         # Obtendo a definição de tarefa atual do serviço
@@ -16,20 +16,20 @@ def lambda_handler(event, context):
         if not response['services']:
             print("Service not found in the cluster.")
             return {"statusCode": 500, "body": "Service not found in the cluster."}
-        
+
         current_task_definition = response['services'][0]['taskDefinition']
         print(f"Current task definition: {current_task_definition}")
-        
+
         # Obtendo a revisão mais recente da definição de tarefa
         response = client.list_task_definitions(familyPrefix=family, status='ACTIVE', sort='DESC')
         print(f"Task definitions response: {response}")
         if not response['taskDefinitionArns']:
             print("No active task definitions found.")
             return {"statusCode": 500, "body": "No active task definitions found."}
-        
+
         new_task_definition = response['taskDefinitionArns'][0]
         print(f"New task definition: {new_task_definition}")
-        
+
         # Verificando se há uma nova revisão disponível
         if current_task_definition == new_task_definition:
             print("Nenhuma nova versão disponível.")
@@ -54,26 +54,15 @@ def lambda_handler(event, context):
         }
         http = urllib3.PoolManager()
         r = http.request("POST", slack_webhook_url, body=json.dumps(slack_message), headers={"Content-Type": "application/json"})
-        
+
         # Verificar se a mensagem foi enviada com sucesso
         if r.status != 200:
             print(f"Failed to send message to Slack. Status code: {r.status}")
             return {"statusCode": r.status, "body": "Failed to send message to Slack."}
-        
-        # Processar a resposta do usuário
-        if event.get('value') == 'yes':
-            # Atualizar o serviço ECS
-            response = client.update_service(cluster=cluster_name, service=service_name, taskDefinition=new_task_definition)
-            # Extrair a revisão da task definition
-            task_revision = new_task_definition.split(":")[-1]
-            # Enviar mensagem de confirmação ao Slack
-            message = f"Serviço atualizado com sucesso para a versão {task_revision}."
-            slack_message = {"text": message}
-            http.request("POST", slack_webhook_url, body=json.dumps(slack_message), headers={"Content-Type": "application/json"})
-            return {"statusCode": 200, "body": "Service updated successfully."}
-        
-        return {"statusCode": 200, "body": "Waiting for user confirmation."}
-    
+
+        # Se a mensagem foi enviada com sucesso, retornar status 200
+        return {"statusCode": 200, "body": "Mensagem enviada para o Slack com o botão de atualização."}
+
     except Exception as e:
         print(f"An error occurred: {str(e)}")
         return {"statusCode": 500, "body": f"An error occurred: {str(e)}"}
--- cloudformation/environment/FARGATE/lambda/update-ecs.py
+++ cloudformation/environment/FARGATE/lambda/update-ecs.py
@@ -1,63 +1,42 @@
-import json
 import boto3
 
 def lambda_handler(event, context):
     try:
-        # Log do evento recebido para diagnóstico
-        print("Received event: " + json.dumps(event, indent=2))
-
-        # Verificar se 'body' está presente no evento
-        if 'body' in event:
-            # Decodificar o corpo da solicitação
-            body = json.loads(event['body'])
-
-            # Verificar se há um payload no corpo da solicitação
-            if 'payload' in body:
-                payload = json.loads(body['payload'])
-            else:
-                raise ValueError("No payload found in request body")
-        else:
-            raise ValueError("Body not found in the event")
-
-        # Verificar se há ações no payload
-        if 'actions' in payload and len(payload['actions']) > 0:
-            actions = payload['actions'][0]['value']
-            action_details = json.loads(actions)
-        else:
-            raise ValueError("No actions found in payload")
-
-        cluster_name = action_details['cluster']
-        service_name = action_details['service']
-
+        # Configurações do ECS
         client = boto3.client('ecs')
+        cluster_name = 'estudo-dev-cluster1'
+        service_name = 'estudo-dev-app-game10'
+        family = 'estudo-dev-app-game10'
 
-        # Obter a última revisão disponível
-        task_definitions = client.list_task_definitions(
-            familyPrefix=service_name,
-            status='ACTIVE',
-            sort='DESC'
+        # Obtendo a revisão mais recente da definição de tarefa
+        response = client.list_task_definitions(familyPrefix=family, status='ACTIVE', sort='DESC')
+        if not response['taskDefinitionArns']:
+            print("No active task definitions found.")
+            return {"statusCode": 500, "body": "No active task definitions found."}
+
+        new_task_definition = response['taskDefinitionArns'][0]
+        print(f"New task definition: {new_task_definition}")
+
+        # Atualizar o serviço ECS com a nova definição de tarefa
+        response = client.update_service(
+            cluster=cluster_name,
+            service=service_name,
+            taskDefinition=new_task_definition
         )
 
-        if 'taskDefinitionArns' in task_definitions and len(task_definitions['taskDefinitionArns']) > 0:
-            latest_revision_arn = task_definitions['taskDefinitionArns'][0]
-
-            # Atualizar o serviço para usar a última revisão
-            client.update_service(
-                cluster=cluster_name,
-                service=service_name,
-                taskDefinition=latest_revision_arn
-            )
+        # Verificar se a atualização foi bem-sucedida
+        if response['service']['status'] == 'ACTIVE':
+            task_revision = new_task_definition.split(":")[-1]
+            return {
+                'statusCode': 200,
+                'body': f'Serviço atualizado com sucesso para a versão {task_revision}.'
+            }
         else:
-            raise ValueError("No active task definitions found")
-
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Service updated successfully')
-        }
+            return {
+                'statusCode': 500,
+                'body': 'Falha ao atualizar o serviço ECS.'
+            }
 
     except Exception as e:
-        print(f'An error occurred: {str(e)}')
-        return {
-            'statusCode': 500,
-            'body': f'An error occurred: {str(e)}'
-        }
+        print(f"An error occurred: {str(e)}")
+        return {"statusCode": 500, "body": f"An error occurred: {str(e)}"}
