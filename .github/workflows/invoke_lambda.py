a67314f5d12d954aab742b9d8bc3affe73f3d299
Celso <celso@celsonunes.com.br>
2024-08-01 13:33:07 -0300

    update

--- .github/workflows/invoke_lambda.py
+++ .github/workflows/invoke_lambda.py
@@ -1,20 +1,34 @@
 import boto3
 import requests
-from requests.auth import AWS4Auth
-import os
+from requests_aws4auth import AWS4Auth
 
-region = os.environ['AWS_REGION']
-service = 'lambda'
-credentials = boto3.Session().get_credentials()
-auth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+# Configurar as credenciais e a região AWS
+session = boto3.Session()
+credentials = session.get_credentials().get_frozen_credentials()
+aws_region = "us-east-1"
+service = "lambda"
 
-headers = {"Content-Type": "application/json"}
-data = {
-    "status": "success",
-    "repository": os.environ['REPOSITORY_NAME'],
-    "version": os.environ['IMAGE_VERSION'],
-    "actor": os.environ['GITHUB_ACTOR']
-}
+# Configurar a autenticação AWS4
+auth = AWS4Auth(credentials.access_key, credentials.secret_key, aws_region, service, session_token=credentials.token)
 
-response = requests.post(os.environ['FUNCTION_URL'], json=data, headers=headers, auth=auth)
-print(response.text)
+def lambda_handler(event):
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "status": "success",
+        "repository": event["repository"],
+        "version": event["version"],
+        "actor": event["actor"]
+    }
+
+    response = requests.post(event["FUNCTION_URL"], json=data, headers=headers, auth=auth)
+    print(response.text)
+
+if __name__ == "__main__":
+    # Exemplo de evento para testar localmente
+    event = {
+        "repository": "example-repo",
+        "version": "11",
+        "actor": "example-actor",
+        "FUNCTION_URL": "https://6fray2qnjbnte5jruhxff5ijoi0huqmm.lambda-url.us-east-1.on.aws/"
+    }
+    lambda_handler(event)
